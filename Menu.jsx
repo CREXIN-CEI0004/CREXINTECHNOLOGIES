bd601785aa063b49f96a4a887be5a336f2fd4ea5
Shanmukha Tonangi <109782324+CREXIN-CEI0004@users.noreply.github.com>
2022-08-25 16:25:12 +0530

    Update Menu.jsx

--- Menu.jsx
+++ Menu.jsx
@@ -14,7 +14,7 @@ import { NavLink } from 'react-router-dom'
 
 const Menu = () => {
   return (
-    <div className='menupage' style={{backgroundColor:"#13708a"}} >
+    <div className='menupage' style={{backgroundImage:"url(images/backround.png)"}} >
 
 
 
